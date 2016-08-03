1da5c1c1e864cb702110b2ce43110d0c7ec2c6d8
Ramesh Sridharan <rameshvs@csail.mit.edu>
2016-08-03 10:05:40 +0300

    fix database filename

--- initialize.py
+++ initialize.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from database_setup import Base, Users, Books, Authors, Reviews
 from datetime import datetime
 
-engine = create_engine('sqlite:///crudlab.db')
+engine = create_engine('sqlite:///project.db')
 Base.metadata.create_all(engine)
 Base.metadata.bind = engine
 
