bd9322b4a34ae6399fed7ade3a84b01c35a0aa39
george17 <george17@meet.mit.edu>
2016-08-04 22:32:17 +0300

    Updated multiple pages. Adding styling, along with books to the database

--- initialize.py
+++ initialize.py
@@ -13,13 +13,19 @@ session = DBSession()
 
 #You can add some starter data for your database here.
 
-comedy = Genre(name="Comedy")
-action = Genre(name="Action")
-drame = Genre(name="Drama")
+#comedy = Genre(name="Comedy")
+#action = Genre(name="Action")
+#drame = Genre(name="Drama")
 
-session.add(comedy)
-session.add(action)
-session.add(drame)
+#dob=datetime(year=1941, month=3, day= 13)
+#author1=Authors(name="Mahmoud Darwish", dob=dob, nat="Palestinian", lang="Arabic")
+book=Books(name="Unfortunately, it was paradise", authorid=1, lang="Arabic", nat="Palestinian", picture="paradise.jpeg")
+
+#session.add(comedy)
+#session.add(action)
+#session.add(drame)
+session.add(book)
+#session.add(author1)
 
 
 
