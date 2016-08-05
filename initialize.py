d40df118ead3b0c389aa94ead601932009f29147
george17-meet <george17@meet.mit.edu>
2016-08-05 17:40:53 +0300

    New book added, along with a fix

--- initialize.py
+++ initialize.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
-from database_setup import Base, Users, Books, Authors, Reviews, Genre
+from database_setup import Base, Users, Books, Authors, Reviews, Genre, BookToGenre, UserToGenre, association_table
 from datetime import datetime
 
 engine = create_engine('sqlite:///project.db')
@@ -13,19 +13,23 @@ session = DBSession()
 
 #You can add some starter data for your database here.
 
-#comedy = Genre(name="Comedy")
-#action = Genre(name="Action")
-#drame = Genre(name="Drama")
+comedy = Genre(name="Comedy")
+action = Genre(name="Action")
+drame = Genre(name="Drama")
 
-#dob=datetime(year=1941, month=3, day= 13)
-#author1=Authors(name="Mahmoud Darwish", dob=dob, nat="Palestinian", lang="Arabic")
+dob=datetime(year=1941, month=3, day= 13)
+author1=Authors(name="Mahmoud Darwish", dob=dob, nat="Palestinian", lang="Arabic", picture="Mahmoud-Darwish.jpg")
 book=Books(name="Unfortunately, it was paradise", authorid=1, lang="Arabic", nat="Palestinian", picture="paradise.jpeg")
-
-#session.add(comedy)
-#session.add(action)
-#session.add(drame)
+book2=Books(name="Journal of an Ordinary Grief", authorid=1, lang="Arabic", nat="Palestinian", picture="grief.jpeg")
+bookrel=BookToGenre(book_id=1, genre_id=3)
+book2rel=BookToGenre(book_id=2, genre_id=3)
+session.add(comedy)
+session.add(action)
+session.add(drame)
 session.add(book)
-#session.add(author1)
+session.add(book2)
+session.add(author1)
+session.add(bookrel)
 
 
 
